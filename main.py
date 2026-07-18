cf01068ecff12384ceee72bf34277affac072986
Genius0515 <50777975+Genius0515@users.noreply.github.com>
2026-07-18 13:25:37 +0800

    First upload files
    upload files

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+專案名稱:開始上傳第一個Github專案
+專案說明:嘗試上傳專案檔案
+"""
+import sys
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+if __name__ == '__main__':
+
+    print_hi('Python')
+
+    sys.exit(0)
