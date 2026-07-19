7c4b7ee8e9df73620445a6d99145e177d120de2c
Genius0515 <50777975+Genius0515@users.noreply.github.com>
2026-07-19 23:22:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 """
 Last Update Date:20260719
-Project Name:Start to upload first Github project
-Project Description:Try to use requirements.txt standard to give other person project program
+Python Version:3.14.5
+Project Name:Start to upload first Github project.
+Project Description:
+    1.Try to use requirements.txt standard to give other person project program.
+    2.Test to run in Linux by WSL Ubuntu.
 """
 import sys
 
